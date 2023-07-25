67bae633ece6adb939ec38d245908e8e41b7f97f
Jesus Vallez Gomis <jesusvg91@gmail.com>
2023-07-25 21:26:35 +0200

    feat(theme): added prop to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function astroComponentsPlugin({ addComponents }) {
+      addComponents({
+        '.h-my-screen': {
+          height: ['100vh', '100dvh'],
+        },
+      })
+    }),
+  ],
 }
